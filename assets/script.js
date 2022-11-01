168acb4590e559bed6aa7a5b45d41217868b25b6
Jules Joseph <julesj93@gmail.com>
2022-11-01 15:28:27 -0400

    Added new variables to JS and matched them on html

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var submit =document.querySelector("#submit");
+var timer =document.querySelector("#timer");
+var scores =document.querySelector("#scores");
+var submitButton = document.querySelector(".submit");
+
+
+
